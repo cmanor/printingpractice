eff0cbef17810caadd25960f483b9c1ac67ae82d
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-02 11:41:31 -0400

    all done

---
+++ printingpractice/src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+  public static void main(String[] args) throws Exception {
+    String s="Waddup"+" TA's";
+    System.out.println(s);
+  }
+}
---
+++ printingpractice/src/Vacation.java
@@ -0,0 +1,8 @@
+public class Vacation {
+  public static void main(String[] args) throws Exception {
+
+      System.out.println("I'd like to visit");
+      System.out.println("Paris");
+      System.out.println("France");
+  }
+}
