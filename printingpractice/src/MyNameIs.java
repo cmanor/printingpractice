5266890f0a227823a7338819577d03159f057cf2
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-02 11:30:44 -0400

    first assign done

---
+++ printingpractice/src/MyNameIs.java
@@ -0,0 +1,8 @@
+public class MyNameIs {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("My name is what?");
+        System.out.println("My name is who?");
+        System.out.println("My name is chika chika Slim Shady");
+    }
+}
